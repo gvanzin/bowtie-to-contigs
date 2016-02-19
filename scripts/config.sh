444309a4c7cc613fec892f2c0b71e4f206f8bf95
Scott G. Daniel <scottdaniel25@gmail.com>
2016-02-19 16:09:37 -0600

    updating for slurm / tacc

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,26 +6,26 @@ export CWD=$PWD
 export SCRIPT_DIR=$CWD
 
 #your root working directory (not your $HOME but maybe it is)
-export COMMON_DIR="/gsfs1/rsgrps/bhurwitz/scottdaniel"
+export COMMON_DIR="/work/03859/sdaniel"
 
 #root project dir
 export PRJ_DIR="$COMMON_DIR/bowtie-to-contigs"
 
 #input contigs / also where bowtie2db will be
-export CONTIG_DIR="$COMMON_DIR/spades_output"
+export CONTIG_DIR="$COMMON_DIR/spades_output/K21"
+export CONTIGS="$CONTIG_DIR/final_contigs.fasta"
 export BOWTIE2_DB="$CONTIG_DIR/final_contigs"
 
 #input fasta
 #export FASTA_DIR="$PRJ_DIR/fasta"
-export FASTA_DIR="/gsfs1/rsgrps/bhurwitz/kyclark/mouse/data/screened"
+#export FASTA_DIR="/gsfs1/rsgrps/bhurwitz/kyclark/mouse/data/screened"
 
 #place to store split-up fasta (step 00)
-export ALT_DATA="/rsgrps/bhurwitz/scottdaniel/uproc_shortread_to_pfam/data"
-export SPLIT_FA_DIR="$ALT_DATA/split"
+export SPLIT_FA_DIR="$COMMON_DIR/split_fasta"
 #export SPLIT_FA_DIR="$PRJ_DIR/fasta-split"
 
 #place that original fastq's are
-export FASTQ_DIR="/gsfs1/rsgrps/bhurwitz/hurwitzlab/data/raw/Doetschman_20111007/all"
+export FASTQ_DIR="$COMMON_DIR/fastq"
 
 #place to store split up fastq's for searching later
 export SPLIT_FQ_DIR="$PRJ_DIR/split-fastq"
