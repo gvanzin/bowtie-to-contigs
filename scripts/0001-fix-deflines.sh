a79cbbaf66c8360aa9a3b8c9c5a3e27a3222805f
Scott G. Daniel <scottdaniel25@gmail.com>
2016-04-26 17:45:49 -0500

    yea

--- scripts/0001-fix-deflines.sh
+++ scripts/0001-fix-deflines.sh
@@ -9,6 +9,8 @@ else
     exit 12385
 fi
 
+#For MEGAHIT
+sed -i 's/ .*$//g' $CONTIGS
 
-
+#custom for patric genomes
 #sed -i -e 's/ .*\. //g' -e 's/[\[]//g' -e 's/\]//g' -e 's/\s/-/g' -e 's/>accn|/>accn-/g' -e 's/|//g' $CONTIGS
