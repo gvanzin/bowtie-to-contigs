6e16b3e0a20a094f357dcd3402589022a87046ec
Scott G. Daniel <scottdaniel25@gmail.com>
2016-05-19 16:21:28 -0700

    script for interactive access to ocelote

---
+++ scripts/ocelote-interactive.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#PBS -W group_list=bhurwitz
+#PBS -q standard
+#PBS -l select=1:ncpus=6:mem=252gb:pcmem=42gb
+#PBS -l walltime=12:00:00
+#PBS -l cput=12:00:00
+#PBS -M scottdaniel@email.arizona.edu
+#PBS -m bea
+
+
