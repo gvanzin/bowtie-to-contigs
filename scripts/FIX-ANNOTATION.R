5cd90100342342ec55ed883e0500e7861c9db97d
Scott G. Daniel <scottdaniel25@gmail.com>
2016-05-19 16:20:39 -0700

    new file to fix annotation as part of step 002

---
+++ scripts/FIX-ANNOTATION.R
@@ -0,0 +1,7 @@
+setwd("/rsgrps/bhurwitz/scottdaniel/make-custom-patric-metagenome/data")
+
+my_annotation = read.delim("patric-annotation-for-contigs.tab",colClasses="character")
+
+my_annotation$prot<-paste('prot',row(my_annotation)[,1],sep = '')
+
+write.table(x = my_annotation,file = "patric-annotation-for-contigs.tab",quote = F,sep = '\t',row.names = F,col.names = F)
