864c4dc6f2b30860abef0825b28c0571df5f41bc
Scott G. Daniel <scottdaniel25@gmail.com>
2016-05-22 10:39:05 -0700

    more commands to fix annotation

--- scripts/FIX-ANNOTATION.R
+++ scripts/FIX-ANNOTATION.R
@@ -1,5 +1,10 @@
 setwd("/rsgrps/bhurwitz/scottdaniel/make-custom-patric-metagenome/data")
 
+system("head -1 patric-annotation-for-contigs.tab > header.temp")
+system("egrep -v '^prot\s.+' patric-annotation-for-contigs.tab > temp.tab")
+system("cat header.temp temp.tab > temp2.tab")
+system("mv temp2.tab patric-annotation-for-contigs.tab")
+
 my_annotation = read.delim("patric-annotation-for-contigs.tab",colClasses="character")
 
 my_annotation$prot<-paste('prot',row(my_annotation)[,1],sep = '')
